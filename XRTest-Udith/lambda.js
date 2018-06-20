06b3ea83968ceff1131a8d1ac181fdcf2c0697f9
sigmaidetest <35022586+sigmaidetest@users.noreply.github.com>
2018-06-20 13:58:09 +0530

    swd

--- XRTest-Udith/lambda.js
+++ XRTest-Udith/lambda.js
@@ -1,4 +1,7 @@
 let AWS = require('aws-sdk');
+var AWSXRay = require('aws-xray-sdk');
+AWSXRay.captureHTTPsGlobal(require('http'));
+var http = require('http');
 let SL_TWITTER = require('slappforge-sdk-twitter');
 let twitterClients = require('./TwitterClients');
 const twitter = new SL_TWITTER.Twitter();
@@ -9,6 +12,7 @@ exports.handler = function (event, context, callback) {
     twitter.searchTweets(twitterClients['twClient'], { "q": "serverless", "count": "5" })
         .then(response => {
             let data = response.data;
+            console.log(data);
         })
         .catch(err => console.log(err));
 
